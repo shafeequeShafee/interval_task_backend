4db36b42603a9162596faa4c4f2a289fb5bb24e1
shafeequeShafee <shafeequeottakath1997@gmail.com>
2024-06-29 14:32:18 +0530

    changes added

--- service/taskControllerService.js
+++ service/taskControllerService.js
@@ -1,9 +1,8 @@
-const { query } = require("express");
+const { query, response } = require("express");
 const mysqlConnection = require("../config/mysqlConnection");
 
-const getAllTaskService = async (req, res, next) => {
+const mysqlQueryExecution = (query) => {
   return new Promise((resolve, reject) => {
-    let query = "select * from tasks where is_active=1 order by id desc";
     mysqlConnection.query(query, (err, rows) => {
       if (!err) {
         resolve(rows);
@@ -13,22 +12,96 @@ const getAllTaskService = async (req, res, next) => {
     });
   });
 };
+const getAllTaskService = async (req, res, next) => {
+  try {
+    let query = "select * from tasks where is_active=1 order by id desc";
+    let result = await mysqlQueryExecution(query);
+    return result;
+  } catch (err) {
+    next(err);
+  }
+};
 
 const updateTaskListService = async (req, res, next) => {
-  return new Promise((resolve, reject) => {
-    let query;
-    console.log("hiii");
-    // mysqlConnection.query(query,(err,rows)=>{
-    //     if(!err){
+  try {
+    let taskItems = req.body;
+    if (taskItems?.length > 0) {
+      let updateQuery = "";
+      let insertQuery = "";
+      let duplicateTask = [];
+      taskItems.map((data) => {
+        let imageName = data?.image.toString();
+        let currentItem = {
+          id: data?.id,
+          date: data?.date,
+          heading: data?.heading,
+          description: data?.description,
+          time: data?.time,
+          priority: data?.priority,
+          image: imageName,
+          flag: data?.flag,
+        };
+        if (currentItem?.id !== "" && currentItem?.id !== undefined) {
+          duplicateTask.push(currentItem?.id);
+          if (currentItem?.flag === true) {
+            updateQuery += `update tasks set heading='${currentItem?.heading}', 
+              description='${currentItem?.description}',
+               priority='${currentItem?.priority}',time='${currentItem?.time}',image='${currentItem?.image}',
+               date='${currentItem?.date}',is_active='1' where id='${currentItem?.id}';
+              `;
+          }
+        } else {
+          insertQuery += `insert into tasks (heading,description,priority,time,date,image,is_active) 
+            values ('${currentItem?.heading}','${currentItem?.description}','${currentItem?.priority}',
+            '${currentItem?.time}','${currentItem?.date}','${currentItem?.image}','1');
+            `;
+        }
+      });
 
-    //     }
-    //     else{
-    //         reject(err)
-    //     }
-    // })
-  });
+      if (duplicateTask?.length > 0) {
+        updateQuery += `update tasks set is_active='0' where id NOT IN ('${duplicateTask.join(
+          ","
+        )}');`;
+      }
+      let query = updateQuery + insertQuery;
+      console.log(query)
+      let result = await mysqlQueryExecution(query);
+      if (result) {
+        return {
+          status: 200,
+          error: false,
+          message: "Successfully updated",
+        };
+      } else {
+        return {
+          status: 200,
+          error: false,
+          message: "No data found",
+        };
+      }
+    } else {
+      let query = `update tasks set is_active='0'`;
+      let result = await mysqlQueryExecution(query);
+      if (result?.changedRows > 0) {
+        return {
+          status: 200,
+          error: false,
+          message: "Updated Successfully",
+        };
+      } else {
+        return {
+          status: 200,
+          error: false,
+          message: "No data found",
+        };
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
 };
+
 module.exports = {
   getAllTaskService,
-  updateTaskListService
+  updateTaskListService,
 };

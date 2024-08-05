1782a43615215afa3dd94ce75a3ea1e028ada344
shafeequeShafee <shafeequeottakath1997@gmail.com>
2024-08-06 00:03:59 +0530

    changes added

--- index.js
+++ index.js
@@ -40,12 +40,67 @@ app.get("/api/user/:id/:username", (req, res) => {
 });
 
 ///////// => query parameter
-// => /api/products?keyOne=valueOne&&keyTwo=valueTwo
+// => /api/products?keyOne=valueOne&keyTwo=valueTwo
 app.get("/api/products", (req, res) => {
   console.log(req.query?.keyOne, req.query?.keyTwo);
 });
-app.get;
+//////////////////
+
+// let req={
+//   query:{
+//       name:"sfq",
+//       age:27
+//   },
+//   params:{
+//       id:1,
+//       page:10
+//   }
+// }
+// let {query:{
+//   name,age
+// },params}=req
+
+// console.log("params",params)  =>params { id: 1, page: 10 }
+// console.log("name",name) =>name sfq
+
+////////////////
+
+/////// => post request ,  body => payload
+/// => post requestil req til ninnu data  json formatil pass cheyyan middleware vennam
+////=> app.use() => used to register middleware
+///=> app.use(express.json());
+
+app.post("/api/users", (req, res) => {
+  //console.log(req.body)=> undefined
+  return res.status(200).send({
+    msg: "hiiii",
+  });
+});
+
+///// status(201) => created
 app.use(express.json());
+
+app.post("/api/users/create",(req,res)=>{
+   return res.status(201).send({
+    msg:"created"
+   })
+})
+
+
+/////// put request & patch are used to update data but technically different on how u update data
+
+//// update entire resource(record)
+app.put("",(req,res)=>{
+
+})
+
+//// patch request updata a record partially
+app.patch("",(req,res)=>{
+  
+})
+
+
+//// delete => used to delete record from 
 app.use(express.urlencoded({ extended: true }));
 app.use("/task-router", taskRouter);
 app.all("*", (req, res, next) => {
